3d057855e508b365df0b2ce926cf006aacfea5df
VINAYAK B K <vinayakbk5576@gmail.com>
2026-04-08 18:33:50 +0530

    Graph and UI upgardes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+import matplotlib.pyplot as plt
 with open ("Categories.json","r") as file:
     data = json.load(file)
 with open ("budget_planning.json","r") as file:
@@ -9,7 +10,6 @@ with open ("budget_planning.json","r") as file:
 def save_data():
     with open ("Categories.json","w") as file:
         json.dump(data,file,indent=4)
-
         
 assign_category = ['Travel','College','Fun','Food','College event','Necessities', 'My stuff']
 assign_month = ['January', 'February', 'March','April','May','June','July','August','September','October','November','December']
@@ -26,10 +26,7 @@ def choose_month():
         return Month 
     else:
         print('Please enter a Number from Valid range')
-        return choose_month()
-    
-        
-    
+        return choose_month() 
 
 def add_money(month_I):
     m=assign_month[month_I]
@@ -89,7 +86,6 @@ def show_expense(month_I):
     else:
         print('invalid')
 
-
 def budget_planning(month_I):
     m=assign_month[month_I]
     print("Enter the Serial Number: \n 1.Travel  2.College  3.Fun  4.Food \n 5.College event  6.Necessities  7.My stuff")
@@ -105,7 +101,7 @@ def budget_planning(month_I):
         cat = assign_category[Category]
         
         if cat not in budget_data[m]:
-            print(f"The {Category} doesn't exist in {budget_data} collection")
+            print(f"The {cat} doesn't exist in {budget_data} collection")
             return
         
         max_budget = budget_data[m][cat]["max"]
@@ -124,7 +120,7 @@ def budget_planning(month_I):
             print(f'It\'s within budget for this {m} and the differnce of the remaining amount is: {max_budget - actual}')
             print(f'You have used {usage:.2f}% of your total budget for {cat}')
             if usage >=80 and usage <100:
-                print(f"Your budget for this {cat} in this {m} onth is about to finish.. Spend wisely")
+                print(f"Your budget for this {cat} in this {m} month is about to finish.. Spend wisely")
             if min_budget> actual:
                 print("Your expense way less than minimal expense.. Good job!")      
         else:
@@ -142,7 +138,8 @@ def show_table(month_I):
             
             if category not in budget_data[m]:
                 print(f'{category} not found in Budget Data')
-                continue
+                min_budget = 0
+                max_budget = 0
     
             if category in budget_data[m]:
                 min_budget = budget_data[m][category]["min"]
@@ -168,18 +165,91 @@ def show_table(month_I):
         df= pd.DataFrame(row)
         print(f'{m} Month expenses')
         print(df)
-
-
-    
-    
-while True:
-    print('1.Add expense \n2.Check expense \n3.Budget planning \n4.Show table \n5.Exit')
+        
+def graph():
+    print("Please enter the month (1-12): \n1.January 2.February 3.March 4.April 5.May 6.June \n7.July 8.August 9.September 10.October 11.November 12.December")
+    months_choice_list = []
+    while True:
+        try:
+            Months_choice_input = int(input('Enter the serial no(1-12)[enter 0 for end]: '))
+            if Months_choice_input ==0 and len(months_choice_list) == 0:
+                print("Select at least one month")
+                continue 
+            elif Months_choice_input ==0 and len(months_choice_list) > 0:
+                break
+            
+            elif Months_choice_input >12 or Months_choice_input < 0:
+                print("Please enter a number from the valid range...")
+                continue
+            Months_choice_input = Months_choice_input - 1
+            if assign_month[Months_choice_input] in months_choice_list:
+                print("Duplicate value")
+                continue
+            months_choice_list.append(assign_month[Months_choice_input])
+            
+            
+        except:
+            print("Enter a valid number... ")
+            continue
+    print(months_choice_list, 'these are Your selected months')
     try:
-        USER_WISH = int(input('Wt are Wishing to go for?'))
+        print("Category options: \n 1.Travel  2.College  3.Fun  4.Food \n 5.College event  6.Necessities  7.My stuff") 
+        cat_choice_input = int(input("Enter the Serial Number: ")) 
+        if cat_choice_input<1 or cat_choice_input > 7:
+            print('Invalid choice')
+            return
+        cat_choice_input = cat_choice_input -1
+        
+    except:
+        print("Enter a valid number... ")
+        return
+    months_choice_list.sort(key = lambda x : assign_month.index(x))
+    Y = [] 
+    for items in months_choice_list:
+        if assign_category[cat_choice_input] in data[items]:
+            total = sum(data[items][assign_category[cat_choice_input]])
+            Y.append(total)
+        else:
+            print('Data isnt Available so i have taken 0')
+            Y.append(0)
+    print("Graph Choice:\n1.Line graph \n2.Bar graph")
+    try:
+        Graph_choice = int(input("Enter you graph choice:\n"))     
+    except:
+        print("Enter a valid Number")
+        return
+    try:
+        if Graph_choice == 1:
+            plt.figure(figsize=(8,5))
+            plt.plot(months_choice_list,Y, marker = 'o',linestyle = '-')
+            plt.title(f'Expenses on {assign_category[cat_choice_input]} in these months')
+            plt.xlabel("Months")
+            plt.ylabel("Expenses")
+            plt.grid(True)
+            plt.show()
+        elif Graph_choice == 2 :
+            plt.figure(figsize=(8,5))
+            plt.bar(months_choice_list,Y)
+            plt.title(f'Expenses on {assign_category[cat_choice_input]} in these months')
+            plt.xlabel("Months")
+            plt.ylabel("Expenses")
+            plt.xticks(rotation=30)
+            plt.tight_layout()
+            plt.show()
+        
+        else:
+            print("Invalid choice")
+    except:
+        print("Invalid Input")
+        return 
+                  
+while True:
+    print('1.Add expense \n2.Check expense \n3.Budget planning \n4.Show table \n5.Show graph \n6.Exit')
+    try:
+        USER_WISH = int(input('Wt are Wishing to go for?\n'))
     except:
         print('INVALID CHOICE')
         continue
-    
     
     if USER_WISH == 1:
         month_I=choose_month()
@@ -194,6 +264,8 @@ while True:
         month_I=choose_month()
         show_table(month_I)
     elif USER_WISH == 5:
+        graph()
+    elif USER_WISH == 6:
         break
     
 print('Done')
